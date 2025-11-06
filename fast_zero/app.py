94b976ec6ca4d2d6df237e61001c74c7c2a329df
EmanuelMello <emanuelmello37@gmail.com>
2025-11-05 22:12:36 -0300

    Testando schemas com o pydantic

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from fast_zero.schemas import Message
+
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Hello World'}
+
+
+@app.get('/hello_world')
+def hello_world() -> Message:
+    return Message(message='Hello World')
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
+from fast_zero.schemas import Message
 
 
 def test_read_root_and_return_hello_world():
@@ -10,3 +11,10 @@ def test_read_root_and_return_hello_world():
     response = client.get('/')  # act
     assert response.status_code == HTTPStatus.OK  # assert
     assert response.json() == {'message': 'Hello World'}  # assert
+
+
+def test_hello_world_and_return_message():
+    client = TestClient(app)
+    response = client.post('/hello_world')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == Message(message='Hello World')
